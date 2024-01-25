1c734c3966ffd8f802b88e9f483a916bea5d6eec
luukschn <luuk.schneider@xs4all.nl>
2024-01-25 13:42:22 +0100

    added code coverage command for frontend unit tests

--- local-app/react-frontend/vitest.config.js
+++ local-app/react-frontend/vitest.config.js
@@ -1,7 +1,13 @@
+import { defineConfig } from 'vitest/config';
+
 export default {
 	root: 'src',
 	// test: /\.(spec|test)\.js$/i,
 	test: {
 		environment: "jsdom",
+		coverage: {
+			provider: 'v8',
+			reportsDirectory: './__test__/coverage'
+		}
 	}
 };
